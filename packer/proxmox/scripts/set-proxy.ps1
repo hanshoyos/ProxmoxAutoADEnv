f650004d7189f3b9742d65eac96063084e8c68c6
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-06-26 17:13:48 -0400

    Delete packer/proxmox directory

--- packer/proxmox/scripts/set-proxy.ps1
+++
@@ -1,3 +0,0 @@
-$reg = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-Set-ItemProperty -Path $reg -Name ProxyServer -Value "x.x.x.x:8080"
-Set-ItemProperty -Path $reg -Name ProxyEnable -Value 1
