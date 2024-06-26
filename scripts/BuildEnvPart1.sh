bbf34f61d0dc629ffb15a3afe607235fd25b5aa5
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-06-26 17:49:05 -0400

    Create BuildEnvPart1.sh

---
+++ scripts/BuildEnvPart1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Function to handle errors
+handle_error() {
+    echo "Error occurred in script at line: $1"
+    exit 1
+}
+
+trap 'handle_error $LINENO' ERR
+
+# Part 1: Update and upgrade the system
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git nano vim tmux curl gnupg software-properties-common mkisofs python3-pip python3-venv
+
+# PROJECT SETUP
+mkdir -p /root/GitProject
+cd /root/GitProject
+git clone https://github.com/hanshoyos/ProxmoxAutoADEnv.git
+
+# Execute the second script
+source /root/GitProject/ProxmoxAutoADEnv/scripts/BuildEnvPart2.sh
