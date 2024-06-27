d020b6b032795029a25e75367d7fdafd45a9aee5
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-06-27 19:25:14 -0400

    Update setup_proxmox.sh

--- scripts/setup_proxmox.sh
+++ scripts/setup_proxmox.sh
@@ -27,7 +27,7 @@ create_venv() {
 install_ansible() {
   echo "Installing Ansible..."
   UBUNTU_CODENAME=jammy
-  wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmor -o /usr/share/keyrings/ansible-archive-keyring.gpg
+  wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmor --yes -o /usr/share/keyrings/ansible-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
   sudo apt update && sudo apt install -y ansible || error_exit "Failed to install Ansible."
 }
@@ -35,7 +35,7 @@ install_ansible() {
 # Function to install Packer and Terraform
 install_packer_terraform() {
   echo "Installing Packer and Terraform..."
-  wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+  wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor --yes | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
   echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
   sudo apt update && sudo apt install -y packer terraform || error_exit "Failed to install Packer and Terraform."
 }
