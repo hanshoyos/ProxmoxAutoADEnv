1d50790d352cb5115a416dd9aa1381bdf5afcb10
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-06-27 19:07:31 -0400

    Update setup_proxmox.sh

--- scripts/setup_proxmox.sh
+++ scripts/setup_proxmox.sh
@@ -26,9 +26,9 @@ create_venv() {
 install_ansible() {
   echo "Installing Ansible..."
   UBUNTU_CODENAME=jammy
-  wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmor -o /usr/share/keyrings/ansible-archive-keyring.gpg
+  wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
-  sudo apt update && sudo apt install -y ansible || error_exit "Failed to install Ansible."
+  sudo apt update && sudo apt install ansible || error_exit "Failed to install Ansible."
 }
 
 # Function to install Packer and Terraform
