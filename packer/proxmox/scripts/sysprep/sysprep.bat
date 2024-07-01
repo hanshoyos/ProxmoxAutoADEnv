f74f13a7158537371f6289861fb4ba6fa2c19c24
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-01 15:02:22 -0400

    Rename packer/proxmox/scripts/sysprep/sysprep.bat to packer/proxmox/WindowsOS/scripts/sysprep/sysprep.bat

--- packer/proxmox/scripts/sysprep/sysprep.bat
+++
@@ -1,3 +0,0 @@
-echo "Start Sysprep"
-cd "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\"
-c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /unattend:cloudbase-init-unattend.xml /quit /shutdown
