2574ee09e3007835947597b7497dd2e49d3d2c61
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 10:32:23 -0400

    Create win_user.ps1

---
+++ packer/ansible/custom_modules/win_user.ps1
@@ -0,0 +1,17 @@
+param (
+    [string]$name,
+    [string]$password,
+    [string[]]$groups,
+    [string]$state
+)
+
+$secure_password = ConvertTo-SecureString $password -AsPlainText -Force
+
+if ($state -eq 'present') {
+    New-LocalUser -Name $name -Password $secure_password -PasswordNeverExpires
+    foreach ($group in $groups) {
+        Add-LocalGroupMember -Group $group -Member $name
+    }
+} else {
+    Remove-LocalUser -Name $name
+}
