249f45ac7e96fb1eb5f9c4bef4ee914f5e699224
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 10:31:00 -0400

    Create win_feature.ps1

---
+++ packer/ansible/custom_modules/win_feature.ps1
@@ -0,0 +1,14 @@
+param (
+    [string[]]$name,
+    [bool]$include_management_tools = $false,
+    [bool]$include_sub_features = $false,
+    [string]$state
+)
+
+foreach ($feature in $name) {
+    if ($state -eq 'present') {
+        Install-WindowsFeature -Name $feature -IncludeManagementTools:$include_management_tools -IncludeAllSubFeature:$include_sub_features
+    } else {
+        Remove-WindowsFeature -Name $feature
+    }
+}
