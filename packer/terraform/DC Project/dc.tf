28007d2379c8ff6701d24021fcd6c50514acff88
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 10:46:23 -0400

    Update dc.tf

--- packer/terraform/DC Project/dc.tf
+++ packer/terraform/DC Project/dc.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.9.11" # or the latest version available
+      version = "2.9.11" # Ensure this is the latest version
     }
   }
 }
