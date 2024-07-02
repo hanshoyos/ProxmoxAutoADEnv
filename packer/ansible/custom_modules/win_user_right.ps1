cc57af7f06f447cc534da8bae5943f516a98686b
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 10:33:15 -0400

    Create win_user_right.ps1

---
+++ packer/ansible/custom_modules/win_user_right.ps1
@@ -0,0 +1,11 @@
+param (
+    [string]$name,
+    [string[]]$users
+)
+
+foreach ($user in $users) {
+    $sid = (New-Object System.Security.Principal.NTAccount($user)).Translate([System.Security.Principal.SecurityIdentifier]).Value
+    secedit /export /cfg C:\Windows\Temp\secedit.inf
+    (Get-Content C:\Windows\Temp\secedit.inf) -replace ('SeRemoteInteractiveLogonRight = \*') { "SeRemoteInteractiveLogonRight = *$sid" } | Set-Content C:\Windows\Temp\secedit.inf
+    secedit /configure /db secedit.sdb /cfg C:\Windows\Temp\secedit.inf
+}
