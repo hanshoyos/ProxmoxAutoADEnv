95373ae298714d20fd69acab1f0e99a61c2dd677
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 10:30:33 -0400

    Create win_dns_client.ps1

---
+++ packer/ansible/custom_modules/win_dns_client.ps1
@@ -0,0 +1,8 @@
+param (
+    [string[]]$adapter_names,
+    [string[]]$ipv4_addresses
+)
+
+foreach ($adapter in $adapter_names) {
+    Set-DnsClientServerAddress -InterfaceAlias $adapter -ServerAddresses $ipv4_addresses
+}
